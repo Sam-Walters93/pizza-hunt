7806c1d57d5679b483fa472c3a93500207e5b8b6
Sam Walters <stwalters314@gmail.com>
2021-06-15 15:29:15 -0500

    pizza model/controllers/api routes in place

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getAllPizzas,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+router
+  .route('/')
+  .get(getAllPizzas)
+  .post(createPizza);
+
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
